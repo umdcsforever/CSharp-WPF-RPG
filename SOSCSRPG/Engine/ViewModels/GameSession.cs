11fb4eb4edcde536314a88abf880c28256d1659c
Joon Won Park <jpark9493@gmail.com>
2018-12-28 15:01:43 -0500

    up to 04.2: Creating the World

--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Engine.Models;
+using Engine.Factories;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -20,6 +23,11 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
+            CurrentLocation = CurrentWorld.LocationAt(0, -1);
         }
     }
 }
