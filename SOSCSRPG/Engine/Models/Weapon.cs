e44f8648fafcbe02a62f6afe237049364772fdeb
Joon <jpark9493@gmail.com>
2018-12-31 18:01:45 -0500

    up to Lesson 05.1: Creating the Game Item Factory

---
+++ SOSCSRPG/Engine/Models/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamange { get; set; }
+        public int MaximumDamange { get; set; }
+
+        public Weapon(int itemTypeID, string name, int price, int minimumDamage, int maximumDamage)
+            :base(itemTypeID, name, price)
+        {
+            MinimumDamange = minimumDamage;
+            MaximumDamange = maximumDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamange, MaximumDamange);
+        }
+    }
+}
